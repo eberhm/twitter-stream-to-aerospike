d834f5bab4ece73b645e321a2b57a1c1b96e18c6
Eber Herrera <eberhm@gmail.com>
2014-10-31 10:20:52 +0100

    Config file

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    aerospike: {
+        address: '127.0.0.1',
+        port: 3000
+    },
+    twitter: {
+        consumer_key: '~',
+        consumer_secret: '~',
+        access_token: "~",
+        access_token_secret: "~"
+    }
+};
+
+module.exports = config;
